09bccf19aee85ce3a2923ee31bdae3d78df800c5
Mityun <mitki.live@gmail.com>
2023-12-18 02:16:34 +0300

    ref

--- app.py
+++ app.py
@@ -77,19 +77,4 @@ async def on_shutdown():
 
 
 if __name__ == '__main__':
-
-    if "HEROKU" in list(os.environ.keys()):
-
-        executor.start_webhook(
-            dispatcher=dp,
-            webhook_path=config.WEBHOOK_PATH,
-            on_startup=on_startup,
-            on_shutdown=on_shutdown,
-            skip_updates=True,
-            host=WEBAPP_HOST,
-            port=WEBAPP_PORT,
-        )
-
-    else:
-
-        executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=False)
