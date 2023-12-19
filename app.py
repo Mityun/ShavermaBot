d4b9bbbba06f48c944adcb84dc9794642913b988
shlegor <shlegor007@gmail.com>
2023-12-19 04:16:58 +0300

    dock for add.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 from os import getenv
 import config
 
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message, WebAppInfo
@@ -12,49 +12,52 @@ from aiogram.utils.markdown import hbold
 
 TOKEN = config.TOKEN
 
-# All handlers should be attached to the Router (or Dispatcher)
+# Initialize the Dispatcher
 dp = Dispatcher()
+"""
+Dispatcher's class instance 
+"""
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
-    This handler receives messages with `/start` command
+    Handles messages with the /start command
     """
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=[
             [
-                types.KeyboardButton('Сделать заказ', web_app=WebAppInfo('https://ru.wikipedia.org/wiki/%D0%A8%D0%B0%D1%83%D1%80%D0%BC%D0%B0'))
+                types.KeyboardButton('Make an order', web_app=WebAppInfo('https://en.wikipedia.org/wiki/Kebab'))
             ],
         ],
         resize_keyboard=True,
     )
-    
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
     """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    Forwards the received message back to the sender
     """
     try:
         # Send a copy of the received message
         await message.send_copy(chat_id=message.chat.id)
     except TypeError:
-        # But not all the types is supported to be copied so need to handle it
+        # Handles message types that are not supported to be copied
         await message.answer("Nice try!")
 
 
 async def main() -> None:
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    """
+    Initializes the Bot, sets the default parse mode, and starts polling events dispatching
+    """
     bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
-    # And the run events dispatching
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
+
