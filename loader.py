5bf8b065c58a09e93eac229f3fb9c5c76a23a30b
shlegor <shlegor007@gmail.com>
2023-12-19 04:17:27 +0300

    dock for loader.py

--- loader.py
+++ loader.py
@@ -1,3 +1,6 @@
+"""loader of necessary configs + link to temporary data
+
+"""
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from utils.db.storage import DatabaseManager
@@ -5,6 +8,19 @@ from utils.db.storage import DatabaseManager
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+""" creating a bot instance
+
+:param token: config.BOT_TOKEN
+:param token: types.ParseMode.HTML
+"""
+
+
 storage = MemoryStorage()
+""" storage 
+"""
 dp = Dispatcher(bot, storage=storage)
+"""dispatcher instance
+"""
 db = DatabaseManager('data/database.db')
+"""db instance + temporary path
+"""
